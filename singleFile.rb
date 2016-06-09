23b9a684d7a78c8870a37f6df93de543f316fa13
jonathanraxa <jonathanraxa@gmail.com>
2016-06-09 13:32:15 -0700

    affid now handled

--- singleFile.rb
+++ singleFile.rb
@@ -1,26 +1,31 @@
 #!/usr/bin/ruby
 
 cids = Array.new(6)
+oldAffid = ""
+newAffid = ""
 new_cids = Array.new(6)
 your_file = ARGV[0]
 
 # NEW CIDS TO REPLACE OLD ONES
-new1 = ""
-new2 = ""
-new3 = ""
+newCid1 = ""
+newCid2 = ""
+newCid3 = ""
 
-print "CID 1: "
-new1 = $stdin.gets.chomp()
+print "affid: "
+newAffid = $stdin.gets.chomp() 
 
-print "CID 2: "
-new2 = $stdin.gets.chomp()
+# print "CID 1: "
+# newCid1 = $stdin.gets.chomp()
 
-print "CID 3: "
-new3 = $stdin.gets.chomp()
+# print "CID 2: "
+# newCid2 = $stdin.gets.chomp()
 
-new1.to_s
-new2.to_s
-new3.to_s
+# print "CID 3: "
+# newCid3 = $stdin.gets.chomp()
+
+newCid1.to_s
+newCid2.to_s
+newCid3.to_s
 
 puts "Opening file: #{your_file}"
 file = File.open(your_file)
@@ -33,39 +38,70 @@ txt.scan(/cid=\d\d\d\d\d\d/) do |line|
     cids.push(line[4..9])
 end
 
+txt.scan(/affid=\d\d\d\d/) do |line|
+     line.split("=")
+     oldAffid = (line[6..9])
+end
+
+puts 
+puts "Current AFFID: " + oldAffid
+
+
+
+# http://home.mcafee.com/root/campaign.aspx?culture=en-NZ&affid=1144&cid=78979
+txt.scan(/culture=\d\d\d\d/) do |line|
+     line.split("=")
+     affid = (line[6..9])
+end
+
+
+
 file.close
 
 # CREATE AN ARRAY OF CIDS TO BE REPLACED
 # not entirely necessary but may be useful for future use
 temp_cids = cids.reject { |a| a.nil? }
 puts
-old1 = temp_cids[1]
-old2 = temp_cids[2]
-old3 = temp_cids[3]
+oldCid1 = temp_cids[1]
+oldCid2 = temp_cids[2]
+oldCid3 = temp_cids[3]
 
 puts 
 
 puts "Old CIDs to be REPLACED: "
-puts old1
-puts old2
-puts old3
+puts oldCid1
+puts oldCid2
+puts oldCid3
 
 puts
 
 puts "New CIDs: "
-puts new1
-puts new2
-puts new3
+puts newCid1
+puts newCid2
+puts newCid3
 
-# WHERE ALL CIDs ARE REPLACED
+
+
+# HANDLE FOR AFFID
+
+
+
+# HANDLE FOR COUNTRY 
+
+
+
+
+
+#WHERE ALL CIDs ARE REPLACED
 files = Dir[your_file]
 files.each do |filename|
   file_content = IO.read(filename)
 
-	  file_content.gsub!(old1,new1)
-	  file_content.gsub!(old2,new2)
-	  file_content.gsub!(old3,new3)
-
+	  # file_content.gsub!(old1,newCid1)
+	  # file_content.gsub!(old2,newCid2)
+	  # file_content.gsub!(old3,newCid3)
+	
+		file_content.gsub!(oldAffid,newAffid)
 
   output = File.open(filename,'w')
   output.write(file_content)
